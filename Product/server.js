202945ee02a14599d6886e8195c52059567487d8
amitbyn <146044513+Udaykaur@users.noreply.github.com>
2023-10-20 22:15:09 -0400

    Update server.js

--- Product/server.js
+++ Product/server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 require("./app/routes/product.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
